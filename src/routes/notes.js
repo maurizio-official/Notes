51542d6a222b31386f8b82e0d7e10e60a7f50a50
Maurizio <maucap00@gmail.com>
2022-01-04 13:38:55 -0400

    Notes view

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,29 @@
 const router = require('express').Router()
 
+router.get('/notes/add', (req, res) => {
+	res.render('notes/new-note')
+})
+
+router.post('/notes/new-note', (req, res) => {
+	const { title, description } = req.body
+	const err = []
+	if (!title) {
+		err.push({text: 'Please write a title'})
+	}
+	if (!description) {
+		err.push({text: 'Please write a description'})
+	}
+	if (err.length > 0){
+		res.render('notes/new-note', {
+			err,
+			title,
+			description
+		})
+	} else {
+		res.send('Ok')
+	}
+})
+
 router.get('/notes', (req, res) => {
 	res.send('Notes from DB')
 })
