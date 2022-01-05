cd691c7c5b594aa0da1011056430c168f7aafff9
Maurizio <maucap00@gmail.com>
2022-01-04 22:13:02 -0400

    CRUD

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
+const Note = require('../models/Note')
 
 router.get('/notes/add', (req, res) => {
 	res.render('notes/new-note')
 })
 
-router.post('/notes/new-note', (req, res) => {
+router.post('/notes/new-note', async (req, res) => {
 	const { title, description } = req.body
 	const err = []
 	if (!title) {
@@ -20,12 +21,34 @@ router.post('/notes/new-note', (req, res) => {
 			description
 		})
 	} else {
-		res.send('Ok')
+		const newNote = new Note({ title, description })
+		await newNote.save()
+		req.flash('success_msg', 'Note Added Successfully')
+		res.redirect('/notes')
 	}
 })
 
-router.get('/notes', (req, res) => {
-	res.send('Notes from DB')
+router.get('/notes', async (req, res) => {
+	const notes = await Note.find().sort({date: 'desc'}).lean()
+	res.render('notes/all-notes', { notes })
+})
+
+router.get('/notes/edit/:id', async (req, res) => {
+	const note = await Note.findById(req.params.id).lean()
+	res.render('notes/edit-notes', {note})
+})
+
+router.put('/notes/edit-note/:id', async (req, res) => {
+	const { title, description } = req.body
+	await Note.findByIdAndUpdate(req.params.id, { title, description })
+	req.flash('success_msg', 'Note Update Successfully')
+	res.redirect('/notes')
+})
+
+router.delete('/notes/delete/:id', async (req, res) => {
+	await Note.findByIdAndRemove(req.params.id)
+	req.flash('success_msg', 'Note Deleted Successfully')
+	res.redirect('/notes')
 })
 
 module.exports = router
